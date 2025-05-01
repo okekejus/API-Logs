13527a5c67f8a10aef206b47b688fb0ba6bcca83
Justin Okeke, MPH <justinokeke@hotmail.com>
2025-05-01 12:14:03 -0400

    Update google_analytics.py
    Added handling for paginated reports

--- google_analytics.py
+++ google_analytics.py
@@ -25,80 +25,108 @@ property_id = os.getenv('PROPERTY_ID')
 # relevant authentication details
 def get_metrics(starting_date, ending_date):
     
-    # Initialize Client 
     client = BetaAnalyticsDataClient()
-
-    # Set up date range, metrics, and dimensions 
     date_range = DateRange(start_date=starting_date, end_date=ending_date)
+    offset = 0 
+
     metrics = [Metric(name='sessions'), 
-           Metric(name='activeUsers'), 
-           Metric(name='userEngagementDuration'), 
-           Metric(name='totalUsers'), 
-           Metric(name='scrolledUsers'), 
-           Metric(name='screenPageViewsPerUser'), 
-           Metric(name='bounceRate'), 
-           Metric(name='eventCount')
-           ]
+               Metric(name='activeUsers'), 
+               Metric(name='userEngagementDuration'), 
+               Metric(name='totalUsers'), 
+               Metric(name='scrolledUsers'), 
+               Metric(name='screenPageViewsPerUser'), 
+               Metric(name='bounceRate'), 
+               Metric(name='eventCount')
+               ] # need to set this up as list to be entered 
 
     dimensions = [Dimension(name='eventName'), 
-              Dimension(name='country'), 
-              Dimension(name='city'), 
-              Dimension(name='pageTitle'), 
-              Dimension(name='PageLocation'), 
-              Dimension(name='date'), 
-              Dimension(name='percentScrolled'), 
-              Dimension(name='platformDeviceCategory'),
-              Dimension(name='browser')]
+                  Dimension(name='country'), 
+                  Dimension(name='city'), 
+                  Dimension(name='pageTitle'), 
+                  Dimension(name='PageLocation'), 
+                  Dimension(name='date'), 
+                  Dimension(name='percentScrolled'), 
+                  Dimension(name='platformDeviceCategory'),
+                  Dimension(name='browser')]
 
 
     # submit request 
     request = RunReportRequest(
-        property=f"properties/{property_id}",
-        date_ranges=[date_range],
-        metrics=metrics,
-        dimensions=dimensions
-    )
-
-    # Run the report
+            property=f"properties/{property_id}",
+            date_ranges=[date_range],
+            metrics=metrics,
+            dimensions=dimensions,
+            limit = 250000, 
+            offset = 0)
+    
+        # Run the report
     response = client.run_report(request)
-
+    
+    
     data = []
+    
     for row in response.rows:
         row_data = {}
-    # Iterate through dimensions and metrics to create a dictionary for each row
         for i, dimension in enumerate(row.dimension_values):
             row_data[dimensions[i].name] = dimension.value
         for i, metric in enumerate(row.metric_values):
             row_data[metrics[i].name] = metric.value
         data.append(row_data)
+        
 
-    # Create a DataFrame from the list of dictionaries
-    results_df = pd.DataFrame(data)
-
+    total_rows = response.row_count
     
+    if total_rows > 250000: 
+        addon = []
+        reps = ((total_rows - 250000)/250000)
+        iterations = math.ceil(reps)
+    
+        for i in tqdm(range(iterations), desc="Downloading..."): 
+            offset = offset + 250000
+            print
+    
+            request = RunReportRequest(
+            property=f"properties/{property_id}",
+            date_ranges=[date_range],
+            metrics=metrics,
+            dimensions=dimensions,
+            limit = 250000, 
+            offset = offset)
+    
+            response = client.run_report(request)
+    
+            for row in response.rows:
+                row_data = {}
+                for i, dimension in enumerate(row.dimension_values):
+                    row_data[dimensions[i].name] = dimension.value
+                for i, metric in enumerate(row.metric_values):
+                    row_data[metrics[i].name] = metric.value
+                addon.append(row_data)
+    
+    results_df = pd.concat([pd.DataFrame(data), pd.DataFrame(addon)])
+
     # Correcting date format    
     results_df['date'] = results_df['date'].apply(lambda x: datetime.strptime(x, '%Y%m%d').date().strftime('%Y-%m-%d'))
     results_df.columns = ['event_name', 'country', 'city', 'page_title', 'page_location', 'date', 'percent_scrolled', 'platform_device', 'browser', 'sessions', 'active_users', 'user_engagement_duration', 'total_users', 
-                        'scrolled_users', 'screen_page_views_per_user', 'bounce_rate', 'event_count']
+                          'scrolled_users', 'screen_page_views_per_user', 'bounce_rate', 'event_count']
     results_df.sort_values('date', ascending=True, inplace=True)
         # Resetting index
     results_df.reset_index(drop=True, inplace=True)
-
+    
     dtypes = {'sessions': 'int', 
-            'active_users': 'int', 
-            'user_engagement_duration': 'int', 
-            'total_users': 'int',
-            'scrolled_users': 'int',
-            'screen_page_views_per_user': 'float',
-            'bounce_rate': 'float',
-            'event_count': 'int'}   
+              'active_users': 'int', 
+              'user_engagement_duration': 'int', 
+              'total_users': 'int',
+              'scrolled_users': 'int',
+              'screen_page_views_per_user': 'float',
+              'bounce_rate': 'float',
+              'event_count': 'int'}
         # changing types 
     results_df = results_df.astype(dtypes)
-     
-
-
-
+    
     return results_df
+
+    
 
 def main(): 
     results = get_metrics(starting_date, ending_date)
